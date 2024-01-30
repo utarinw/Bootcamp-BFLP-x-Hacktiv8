68d994c409c24527bda5da2ed2d9036d4e087dd2
utarinw <andiutarinurwahidin@gmail.com>
2024-01-30 14:35:11 +0700

    day2

---
+++ Day 2/Challenge/java.js
@@ -0,0 +1,9 @@
+function inputForm(){
+    document.getElementById('getNama').innerHTML = document.getElementById('InputName').value
+    document.getElementById('getRole').innerHTML = document.getElementById('InputRole').value
+    document.getElementById('getAvailability').innerHTML = document.getElementById('InputAvailability').value
+    document.getElementById('getAge').innerHTML = document.getElementById('InputAge').value
+    document.getElementById('getLocate').innerHTML = document.getElementById('InputLocate').value
+    document.getElementById('getYears').innerHTML = document.getElementById('InputYears').value
+    document.getElementById('getEmail').innerHTML = document.getElementById('InputEmail').value
+}
