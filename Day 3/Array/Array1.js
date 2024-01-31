878d5413d4bb84eb44dd2adfe25b68964e35d016
utarinw <andiutarinurwahidin@gmail.com>
2024-01-31 14:25:59 +0700

    1

---
+++ Day 3/Array/Array1.js
@@ -0,0 +1,12 @@
+var myArray = [];
+
+// instrusi 1 & 2
+myArray.push('apple','banana');
+//  instruksi 3
+myArray.pop(0)
+// instruksi 4
+myArray.push('orange');
+// instruksi 5
+myArray.shift()
+
+console.log(myArray)
