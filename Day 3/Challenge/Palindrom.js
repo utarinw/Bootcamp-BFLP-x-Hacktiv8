9b65672d3e06cd77368ee27aaa507bccd5331448
utarinw <andiutarinurwahidin@gmail.com>
2024-01-31 09:10:18 +0700

    Day1

--- Day 3/Challenge/Palindrom.js
+++ Day 3/Challenge/Palindrom.js
@@ -1,15 +1,10 @@
 function cekPalindrom(input) {
-    // Menghapus spasi dan tanda baca, serta mengubah huruf menjadi huruf kecil
     const cleanInput = input.toLowerCase().replace(/[^a-z0-9]/g,'');
-  
-    // Membalikkan string
     const reversedInput = cleanInput.split('').reverse().join('');
-  
-    // Memeriksa apakah string yang dibalik sama dengan string asli
     return cleanInput === reversedInput;
   }
   
-  // Contoh penggunaan fungsi
+  // input contoh //
   const example1 = "Katak";
   const example2 = "Kasur Ini Rusak";
   const example3 = "Level";
